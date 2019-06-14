9c0eb0727b674e62d75173e727bc979176cc78ee
angel_wiki_yodo <wikiyodo@gmail.com>
2019-06-14 09:26:25 +0000

    write logout test

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -15,4 +15,24 @@ describe('Confirm that App.js', ()=>{
         ReactDOM.render(<App />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
+    it('returns empty cart key for cart', ()=>{
+        expect(App.getCartId()).toBe(null);
+    });
+    it('returns empty cart key for cart', ()=>{
+        let cartId = 'cartIDD';
+        localStorage.setItem('cartId', cartId);
+        expect(App.getCartId()).toBe(cartId);
+    });
+});
+
+describe('Authentication', ()=>{
+
+    it('successfully logs user out after login', ()=>{
+        let user = 'User name';
+        let token = 'user token';
+
+        localStorage.setItem('token', JSON.stringify({user, token}));
+        App.logoutUser();
+        expect(App.getUserToken()).toBe('');
+    });
 });
